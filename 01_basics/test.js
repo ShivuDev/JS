b22fea240f0e9b3632745859a92a99c0788b9365
Shivakumara Swamy P <117889446+ShivuDev@users.noreply.github.com>
2025-03-13 22:48:59 +0530

    about const var let

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,7 +1,15 @@
 console.log("shivu")
+<<<<<<< HEAD
+const accountID=5767
+let emaiId="Shivu@google.com"
+var workPlace="Banglore"
+
+console.table([accountID,emaiId,workPlace])
+=======
 const accountID=5767
 let emaiId="Shivu@google.com"
 var workPlace="Banglore"
 let Name
 
 console.table([accountID,emaiId,workPlace,Name])
+>>>>>>> 35e6cc1b82b47569c343491d5332edfd894a2ebb
